30d2f1885e4489d955f72469e874545c540a66cb
Khanh <khanhtranngoccva@gmail.com>
2022-08-03 17:43:51 +0700

    feat: UI revamp

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,58 @@
 import classes from "./Navbar.module.css"
 import Select from "react-select";
 
+const selectStyles = {
+    menu: (provided, state) => ({
+        ...provided,
+        width: "20em",
+        color: "var(--primary)",
+        background: "var(--background)",
+        marginTop: 0,
+    }),
+    control: (provided, state) => ({
+        ...provided,
+        width: "20em",
+        fontSize: "1rem",
+        minHeight: "0",
+        background: "var(--background)",
+        outline: "none",
+        border: "none",
+    }),
+    singleValue: (provided, state) => ({
+        ...provided,
+        width: "20em",
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        background: state.isSelected ? "var(--accent2)" : "var(--background)",
+        color: "var(--primary)",
+    }),
+    valueIndicator: (provided, state) => ({
+        ...provided,
+        background: "var(--accent1)",
+    }),
+    valueContainer: (provided, state) => {
+        return {
+            ...provided,
+        };
+    },
+    dropdownIndicator(provided, state) {
+        return {
+            ...provided,
+            paddingBlock: "0em",
+        }
+    }
+}
+
 export default function Navbar(props) {
     const selectComponents = (props.selectElementConfigs || []).map(selectElementConfig => {
         selectElementConfig.enableCondition ??= () => true;
-        if (selectElementConfig.enableCondition()) {
-            return <Select {...selectElementConfig}></Select>;
-        }
+        return <Select {...selectElementConfig} styles={selectStyles} isDisabled={!selectElementConfig.enableCondition()}></Select>;
     });
 
     return <nav className={classes.navbar}>
-        <form>
+        <span className={classes.logo}>{"</>100Devs"}</span>
+        <form className={classes.sortSelector}>
             {selectComponents}
         </form>
     </nav>
